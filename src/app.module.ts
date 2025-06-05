049cc3debf512234d0c62886b1884d24c17d2de7
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-06-05 01:20:44 +0100

    :spakrles: feat: configuração das variáveis de ambiente e da configuração do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UserModule],
+  imports: [
+    // Import the ConfigModule to manage environment variables
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    // Import the UserModule to handle user-related functionality
+    // Import TypeOrmModule to connect to the database
+    // Ensure you have the TypeORM package installed: npm install @nestjs/typeorm typeorm pg
+    // Set synchronize to true for development, but false in production
+    // This will automatically create database tables based on your entities
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
