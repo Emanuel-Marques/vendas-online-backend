7c78d4e2b1404ccf6eddca8f6ebb986120e5a55c
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-06-06 16:27:27 +0100

    ♻️ refactor: Mudando o tipo retornado pelos controllers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get()
-  async getAllUser() {
+  async getAllUser(): Promise<UserEntity[]> {
     return this.userService.getAllUsers();
   }
 
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser);
   }
 }
