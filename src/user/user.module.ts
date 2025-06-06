0badd0f065918c4291e715a2858c6cae43e07b66
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-06-06 16:27:59 +0100

    ✨ feat: importando entidade no módulo de usuários

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './interfaces/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])], // Import the UserEntity for TypeORM
   controllers: [UserController],
   providers: [UserService],
 })
